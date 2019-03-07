e2ca1e29e909adaf1f8f97cb3117291b7c14d248
denizenofcode <austin.buie@gmail.com>
2019-03-07 08:26:23 -0500

    Added jQuery and a test to determine if it is working/loading

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* Test if jQuery is Working
+window.onload = function() {
+    if (window.jQuery) {
+        // jQuery is loaded
+        alert("jQuery is working.");
+    } else {
+        // jQuery is not loaded
+        alert("jQuery is not working.");
+    }
+}
+*/
